ecac791377425f6048f4324707f71e072fcbd4d5
MMartins-ai <109698815+MMartins-ai@users.noreply.github.com>
2023-01-03 19:20:36 -0300

    Testando a biblioteca Streamlit

---
+++ teste_streamlit.py
@@ -0,0 +1,17 @@
+#Importando bibliotecas
+import streamlit as st
+import pandas as pd
+
+dic_df = {
+    'Nome':['Mateus','Simone','Raimundo','Rosa','Mateus','Mateus','Rosa','Simone','Rosa','Raimundo'],
+    'Valores':[5,6,7,1,3,8,9,4,5,6]
+}
+
+df = pd.DataFrame(dic_df)
+
+st.write("""
+    # Meu Primeiro APP
+    Hello *World*!
+""")
+
+st.line_chart(df)
