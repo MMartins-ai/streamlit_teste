b6547b8f8d22bd42a629afddaaa31f8ec328581e
MMartins-ai <109698815+MMartins-ai@users.noreply.github.com>
2023-01-04 18:40:38 -0300

    Update prevendo_diabetes.py

--- prevendo_diabetes.py
+++ prevendo_diabetes.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
